af4f8840237c1a4c0d9be0b20a56e2a1a612a51f
sararin <sararin@cock.li>
2019-05-29 16:07:48 +0200

    error handling

--- main.c
+++ main.c
@@ -2,26 +2,52 @@
 #include <curl/curl.h>
 #include "str.h"
 
-char *help = "curr-rate BASE [ CONVERT-TO ... ]\n\nmandatory args:\n    BASE        base to convert from\n    CONVERT-TO  converts BASE to following currencies\n";
+char *help_str = "curr-rate [-hl] BASE [ CONVERT-TO ... ]\n\nmandatory args:\n    BASE        base to convert from\n    CONVERT-TO  converts BASE to following currencies\n\noptional args:\n    -l          list of available currencies\n    -h          show help\n";
 
 #define BASE "base="
 #define SYMBOLS "&symbols="
 #define COMMA ","
 
+#define SHOW_PRICE 1
+#define NO_PRICE 0
+
 void generate_url(struct string *s, int argcount, char **fullarg);
-int get_json(struct string *a);
-int read_json(struct string *s);
+int get_json(char *url, int show_prices);
+int read_json(char *resp, int show_prices);
 
 int main(int argc, char **argv) {
-    if (argc < 3) { // needs at least two inputs
-        fprintf(stderr, "%s\n", help);
+    int list = 0;
+    int help = 0;
+
+    if (argc < 2) { // needs at least one input 
+        printf("%s\n", help_str);
         exit(EXIT_FAILURE);
+    }
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "-l"))
+            list = 1;
+        if (!strcmp(argv[i], "-h"))
+            help = 1;
     }
 
     struct string url;
     init_string(&url);
+
+    if (help) {
+        printf("%s\n", help_str);
+        return 0;
+    }
+    if (list) {
+        writefunc("https://api.exchangeratesapi.io/latest?base=USD", 1, strlen("https://api.exchangeratesapi.io/latest?base=USD"), &url);
+        get_json(url.ptr, NO_PRICE);
+        return 0;
+    }
+
     generate_url(&url, argc, argv);
-    get_json(&url);
+    if (!get_json(url.ptr, SHOW_PRICE)) {
+        fprintf(stderr, "get_json() failed\n");
+        exit(EXIT_FAILURE);
+    }
 
     free(url.ptr);
 
@@ -38,11 +64,11 @@ void generate_url(struct string *query, int argcount, char **fullarg) {
         writefunc(fullarg[i], 1, strlen(fullarg[i]), query);
         writefunc(COMMA, 1, strlen(COMMA), query);
     }
-    query->len -= 1;
+    query->len -= 1; //remove the last comma
     query->ptr[query->len] = '\0';
 }
 
-int get_json(struct string *url) {
+int get_json(char *url, int show_prices) {
     CURL *curl;
     CURLcode res;
 
@@ -52,7 +78,7 @@ int get_json(struct string *url) {
         struct string response;
         init_string(&response);
 
-        curl_easy_setopt(curl, CURLOPT_URL, url->ptr); // set rates for now
+        curl_easy_setopt(curl, CURLOPT_URL, url); // set rates for now
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
@@ -61,31 +87,38 @@ int get_json(struct string *url) {
             fprintf(stderr, "curl_easy_perform() failed\n");
         curl_easy_strerror(res);
 
-        read_json(&response); 
+        if (!read_json(response.ptr, show_prices)) {
+            fprintf(stderr, "read_json() failed\n");
+            return 1;
+        }
 
         // string cleanup
         free(response.ptr);
 
         // curl cleanup
         curl_easy_cleanup(curl);
+    } else {
+        return 1;
     }
     
     curl_global_cleanup();
+    return 0;
 }
 
-int read_json(struct string *resp) {
+int read_json(char *resp, int show_prices) {
     struct json_object *jobj; // entire json
     struct json_object *base; // base currency
     struct json_object *rates; // currencies base is oncerted to
 
-    jobj = json_tokener_parse(resp->ptr);
+    jobj = json_tokener_parse(resp);
 
     json_object_object_get_ex(jobj, "base", &base);
     printf("%s 1.00\n", json_object_get_string(base));
 
     json_object_object_get_ex(jobj, "rates", &rates);
     json_object_object_foreach(rates, key, val) {
-        printf("%s %s\n", key, json_object_get_string(val));
+        char *show = show_prices ? json_object_get_string(val) : "";
+        printf("%s %s\n", key, show);
     }
 
     // json cleanup
