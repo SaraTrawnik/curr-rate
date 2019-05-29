386df7af5abaa0b6e87b0d8c412cd113b545b07f
sararin <sararin@cock.li>
2019-05-29 15:39:21 +0200

    working extracting values from json

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ char *help = "curr-rate BASE [ CONVERT-TO ... ]\n\nmandatory args:\n    BASE    
 #define COMMA ","
 
 void generate_url(struct string *s, int argcount, char **fullarg);
+int get_json(struct string *a);
+int read_json(struct string *s);
 
 int main(int argc, char **argv) {
     if (argc < 3) { // needs at least two inputs
@@ -17,49 +19,79 @@ int main(int argc, char **argv) {
     }
 
     struct string url;
+    init_string(&url);
     generate_url(&url, argc, argv);
+    get_json(&url);
 
-    struct json_object *jobj;
+    free(url.ptr);
+
+    return 0;
+}
+
+
+void generate_url(struct string *query, int argcount, char **fullarg) {
+    writefunc("https://api.exchangeratesapi.io/latest?", 1, strlen("https://api.exchangeratesapi.io/latest?"), query);
+    writefunc(BASE, 1, strlen(BASE), query);
+    writefunc(fullarg[1], 1, strlen(fullarg[1]), query);
+    writefunc(SYMBOLS, 1, strlen(SYMBOLS), query);
+    for (int i = 2; i < argcount; i++) {
+        writefunc(fullarg[i], 1, strlen(fullarg[i]), query);
+        writefunc(COMMA, 1, strlen(COMMA), query);
+    }
+    query->len -= 1;
+    query->ptr[query->len] = '\0';
+}
+
+int get_json(struct string *url) {
     CURL *curl;
     CURLcode res;
 
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
     if (curl) {
-        struct string s;
-        init_string(&s);
+        struct string response;
+        init_string(&response);
 
-        curl_easy_setopt(curl, CURLOPT_URL, url.ptr); // set rates for now
+        curl_easy_setopt(curl, CURLOPT_URL, url->ptr); // set rates for now
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
         res = curl_easy_perform(curl);
         if (res != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed\n");
         curl_easy_strerror(res);
 
-        jobj = json_tokener_parse(s.ptr);
-        printf("%s\n", json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY));
-        free(s.ptr);
+        read_json(&response); 
+
+        // string cleanup
+        free(response.ptr);
+
+        // curl cleanup
         curl_easy_cleanup(curl);
     }
     
     curl_global_cleanup();
-    return 0;
 }
 
+int read_json(struct string *resp) {
+    struct json_object *jobj; // entire json
+    struct json_object *base; // base currency
+    struct json_object *rates; // currencies base is oncerted to
 
-void generate_url(struct string *s, int argcount, char **fullarg) {
-    init_string(s);
-    writefunc("https://api.exchangeratesapi.io/latest?", 1, strlen("https://api.exchangeratesapi.io/latest?"), s);
+    jobj = json_tokener_parse(resp->ptr);
 
-    writefunc(BASE, 1, strlen(BASE), s);
-    writefunc(fullarg[1], 1, strlen(fullarg[1]), s);
-    writefunc(SYMBOLS, 1, strlen(SYMBOLS), s);
-    for (int i = 2; i < argcount; i++) {
-        writefunc(fullarg[i], 1, strlen(fullarg[i]), s);
-        writefunc(COMMA, 1, strlen(COMMA), s);
+    json_object_object_get_ex(jobj, "base", &base);
+    printf("%s 1.00\n", json_object_get_string(base));
+
+    json_object_object_get_ex(jobj, "rates", &rates);
+    json_object_object_foreach(rates, key, val) {
+        printf("%s %s\n", key, json_object_get_string(val));
     }
-    s->len -= 1;
-    s->ptr[s->len] = '\0';
+
+    // json cleanup
+    json_object_put(jobj);
+    json_object_put(base);
+    json_object_put(rates);
+
+    return 0;
 }
